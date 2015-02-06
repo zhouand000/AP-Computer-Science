031ac2b854fd2bce2b0a71a2a36d33e985d3cb22
Andrew <zhouand000@isd284.com>
2015-02-06 10:38:52 -0600

    Added to DiceEngine
    Fix ~100

--- MyFirstProject/src/arrays/DiceEngine.java
+++ MyFirstProject/src/arrays/DiceEngine.java
@@ -1,16 +1,129 @@
 package arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
 import java.util.Scanner;
 
 public class DiceEngine {
-	
-	public static void main (String[] args) {
-		
-		// Creates Scanner
+
+	public static void main(String[] args) {
+
+		// Creates Scanner and Random
 		Scanner reader = new Scanner(System.in);
-		
-		// 
-		
+		Random random = new Random(0);
+
+		// Gets number of rolls
+		System.out.print("Number of rolls: \n:");
+		int rolls = reader.nextInt();
+
+		// Gets number of dice
+		System.out.print("Number of Dice: \n:");
+		int dice = reader.nextInt();
+
+		// Creates array
+		int[][] rollArray = new int[rolls][dice];
+
+		// Loops and gets rolls
+		for (int i = 0; i < rolls; i++) {
+
+			// Loops and gets individual dice rolls
+			for (int j = 0; j < dice; j++) {
+
+				// The +1 is to move from [0,5] to [1,6]
+				rollArray[i][j] = random.nextInt(5) + 1;
+
+			}
+
+		}
+
+		// Prints out array
+		System.out.println("The rolls are: ");
+		// System.out.println(Arrays.deepToString(rollArray));
+
+		for (int i = 0; i < rolls; i++) {
+
+			System.out.println(Arrays.toString(rollArray[i]));
+
+		}
+
+		Arrays.sort(rollArray, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] i, int[] j) {
+				// Sorts the array
+				Arrays.sort(i);
+				Arrays.sort(j);
+
+				// If statement to compare the two
+				if (i[0] < j[0]) {
+
+					// Tells sort to put i first
+					return -1;
+
+				} else if (i[0] > j[0]) {
+
+					// Tells sort to put j first
+					return 1;
+
+				} else if (i.length > 1 && j.length > 1) {
+
+					if (i[1] < j[1]) {
+
+						// Tells sort to put i first
+						return -1;
+
+					} else if (i[1] > j[1]) {
+
+						// Tells sort to put j first
+						return 1;
+
+					} else {
+
+						return 0;
+
+					}
+				} else
+					return 0;
+			};
+		});
+
+		// Creates array to print out
+
+		String[][] stringArray = new String[rolls][2];
+
+		// Prints out numbers
+		for (int pass = 0; pass < rolls; pass++) {
+			ArrayList<int[]> temp = new ArrayList(Arrays.asList(rollArray));
+			int total = 1;
+			for (int j = pass + 1; j < rolls-1; j++) {
+				
+				System.out.println(temp.get(pass).equals(temp.get(j)));
+				if (Arrays.toString(temp.get(pass)).equals(Arrays.toString(temp.get(j)))) {
+
+					total++;
+					temp.remove(pass);
+					temp.remove(j);
+				}
+
+			}
+
+			stringArray[pass][0] = Integer.toString(total);
+			stringArray[pass][1] = Arrays.toString(rollArray[pass]);
+
+		}
+		System.out.println("There is/are: ");
+		for (int i = 0; i < rolls; i++) {
+
+			System.out.println(stringArray[i][0] + " x " + stringArray[i][1]);
+
+		}
+
+		// System.out.println(Arrays.deepToString(stringArray));
+
+		// Closes Scanner
+		reader.close();
+
 	}
-	
 }
