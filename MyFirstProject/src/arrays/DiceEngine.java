4c30e2d04bcf1650499d69278a2628746b9a3810
zhouand000 <zhouand000@isd284.com>
2015-02-05 22:30:34 -0600

    Started DiceEngine

---
+++ MyFirstProject/src/arrays/DiceEngine.java
@@ -0,0 +1,16 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class DiceEngine {
+	
+	public static void main (String[] args) {
+		
+		// Creates Scanner
+		Scanner reader = new Scanner(System.in);
+		
+		// 
+		
+	}
+	
+}
