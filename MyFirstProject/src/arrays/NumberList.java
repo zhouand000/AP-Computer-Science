cc701a7e55a87a1950ce5b448c9bd9076d8078ae
zhouand000 <zhouand000@isd284.com>
2015-02-05 22:30:25 -0600

    Finished NumberList

--- MyFirstProject/src/arrays/NumberList.java
+++ MyFirstProject/src/arrays/NumberList.java
@@ -5,19 +5,20 @@ import java.util.Random;
 
 public class NumberList {
 	
-	public static void main(String[] args){
+	public static void main (String[] args) {
 		
 		// Creates Scanner and Random objects
 		Scanner reader = new Scanner(System.in);
 		Random random = new Random(0);
 		
 		// Creates array
-		int[] numbers = new int[100];
+		Integer[] numbers = new Integer[100];
 		
 		// Fills array
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < numbers.length; i++) {
 			
-			// Sets element i in numbers to a value from [1,100]; the + 1 is to move it from [0,99]
+			// Sets element i in numbers to a value from [1,100]; the + 1 is to
+			// move it from [0,99]
 			numbers[i] = random.nextInt(100) + 1;
 			
 		}
@@ -25,61 +26,123 @@ public class NumberList {
 		// Sorts array
 		numbers = selectionSort(numbers);
 		
-		// Initializes count
-		int[] count = new int[100];
+		// Initializes count and set every value to 0
+		int[] count = new int[numbers.length];
+		for (int i : count) {
+			
+			count[i] = 0;
+			
+		}
 		
 		// Counts numbers
-		for (int i = 0; i < 100; i++){
+		for (int i : numbers) {
 			
 			count[numbers[i]]++;
 			
 		}
 		
-		// TODO print in order, print in reverse, print how many of each
+		// This is the array that will get printed out
+		String[] countOutput = new String[numbers.length];
+		for (int i = 0; i < numbers.length; i++) {
+			
+			countOutput[i] = "There "
+					+ (count[i] > 1 || count[i] == 0 ? "are " : "is one ")
+					+ (count[i] == 1 ? "" : (count[i] == 0 ? "zero " : String
+							.valueOf(count[i]) + " ")) + String.valueOf(i)
+					+ (count[i] > 1 || count[i] == 0 ? "s" : "") + ".";
+			
+		}
+		
+		// Prints sorted array
+		printArray(numbers);
+		
+		// Prints reversed array
+		printArray(reverseArray(numbers));
+		
+		// Prints the # of each number
+		printArray(countOutput);
 		
 		// Closes reader
 		reader.close();
 		
 	}
 	
-	public static int[] selectionSort(int[] arr) {
-
+	public static void printArray (int[] array) {
+		
+		// Uses enhanced for loop to print everything
+		for (int i = 0; i < array.length; i++) {
+			
+			System.out.print(array[i] + "\n");
+			
+		}
+		
+	}
+	
+	public static <T> void printArray (T[] array) {
+		
+		// Uses for loop to print everything
+		for (int i = 0; i < array.length; i++) {
+			
+			System.out.print(array[i] + "\n");
+			
+		}
+		
+	}
+	
+	public static int[] reverseArray (Integer[] array) {
+		
+		// Creates temporary array
+		int[] temp = new int[array.length];
+		
+		// Loops through and assigns an element to array.length-i
+		for (int i = 0; i < array.length; i++) {
+			
+			temp[array.length - i - 1] = array[i];
+			
+		}
+		
+		return temp;
+		
+	}
+	
+	public static <T extends Comparable<? super T>> T[] selectionSort (T[] arr) {
+		
 		// Initializes variables used for sorting
-
+		
 		int smallIndex; // Index of the smallest number
 		int pass; // Current sorting pass
 		int j; // Index of smaller loop
 		int length = arr.length; // Length of array
-		int temp;
-
+		T temp;
+		
 		// Loop.
 		// The first pass finds the smallest, the second pass finds the second
 		// smallest, etc.
 		// Will not be affected by duplicates
 		for (pass = 0; pass < (length - 1); pass++) {
-
+			
 			// Sets the index of the smallest to the current pass
 			smallIndex = pass;
-
+			
 			// Finds the smallest element in the pass
 			for (j = pass + 1; j < length; j++) {
-
-				if (arr[j] < arr[smallIndex]) {
-
+				
+				if (arr[j].compareTo(arr[smallIndex]) < 0) {
+					
 					smallIndex = j;
-
+					
 				}
-
+				
 			}
 			// Switches the nth smallest number with the nth number
 			temp = arr[pass];
 			arr[pass] = arr[smallIndex];
 			arr[smallIndex] = temp;
-
+			
 		}
-
+		
 		// Returns the array
 		return arr;
-
+		
 	}
 }
