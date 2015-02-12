4a3dfa84c95cf63af5eee521e81a339906543dcf
Andrew <zhouand000@isd284.com>
2015-02-12 10:38:29 -0600

    Various changes

--- Assignments/src/unitconverter/UnitConverter.java
+++ Assignments/src/unitconverter/UnitConverter.java
@@ -10,8 +10,63 @@ public class UnitConverter {
 		// Creates Scanner
 		Scanner reader = new Scanner(System.in);
 		
+		// Creates mode variable
+		String input;
+		String mode;
+		String unit;
+		
+		do {
+			
+			// Gets input
+			System.out.println("Enter unit type: ");
+			input = reader.nextLine().toLowerCase();
+			
+			// Gets unit type
+			switch (input) {
+				
+				case "length": case "weight": case "power":
+					mode = input;
+					break;
+					
+				default:
+					mode = "invalid";
+					break;
+					
+			}
+			
+		}
+		while (mode != "invalid");
+		
+		
+		
+		
 		reader.close();
+		
+		
 		
 	}
 	
+	public String getUnit(String input) {
+		String unit;
+		// Sets unit
+		switch (input) {
+			
+			case "foot": case "feet": case "ft":  
+				unit = "ft";
+				break;
+			case "mile": case "miles": case "mi":
+				unit = "mi";
+				break;
+			case "pound": case "pounds": case "lb": case "lbs":
+				unit = "lb";
+				break;
+			case "ounce": case "ounces": case "oz":
+				unit = "oz";
+			case "kilometer": case "kilometers": case "km": 
+				unit = "km";
+				break;
+				
+		}
+		return unit;
+	}
 }
