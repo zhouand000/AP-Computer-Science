1be5bbba4829d6f0d5baf1acc2dc5c85db0ebc3f
Andrew <zhouand000@isd284.com>
2015-02-13 10:38:24 -0600

    Added to UnitConverter

--- Assignments/src/unitconverter/UnitConverter.java
+++ Assignments/src/unitconverter/UnitConverter.java
@@ -10,10 +10,15 @@ public class UnitConverter {
 		// Creates Scanner
 		Scanner reader = new Scanner(System.in);
 		
+		// Creates UnitConverter
+		UnitConverter uc = new UnitConverter();
+		
 		// Creates mode variable
 		String input;
-		String mode;
+		String unitType;
 		String unit;
+		String inputUnit;
+		String outputUnit;
 		
 		do {
 			
@@ -25,28 +30,109 @@ public class UnitConverter {
 			switch (input) {
 			
 				case "length":
-				case "weight":
-				case "power":
-					mode = input;
+				case "l":
+				case "distance":
+				case "d":				
+					unitType = "length";
 				break;
-				
+				case "weight":
+				case "w":
+				case "power":
+				case "p":
+					unitType = input;
+				break;
 				default:
-					mode = "invalid";
+					unitType = "invalid";
 				break;
 			
 			}
 			
 		}
-		while (mode != "invalid");
+		while (unitType == "invalid");
+		
+		// Calls readDistanceUnit()
+		inputUnit = uc.readDistanceUnit(reader);
+		// outputUnit = uc.readDistanceUnit(reader);
+		
+		System.out.println("Input number of unit:");
+		double amount = reader.nextDouble();
+		
+		// Does conversions
+		switch (unitType) {
+			
+			case "length":
+				switch (inputUnit) {
+					
+					case "ft":
+						System.out.println(amount + " ft = " + (double) amount / 5280d + " mi");
+					break;
+					case "mi":
+						System.out.println(amount + " mi = " + (double) amount * 5280d + " ft");
+						System.out.println(amount + " mi = " + (double) amount * 0.62137119d + " km");
+					break;
+					case "km":
+						System.out.println(amount + " km = " + (double) amount / 0.62137119d + " mi");
+					break;
+					case "in":
+						System.out.println(amount + " in = " + (double) amount * 2.54d + " cm");
+					break;
+					case "cm":
+						System.out.println(amount + " cm = " + (double) amount / 2.54d + " in");
+					break;
+				}
+				break;
+			case "weight":
+				switch (inputUnit) {
+					
+					
+					
+				}
+			
+		}
+		
+		
+		System.out.println();
 		
 		reader.close();
 		
 	}
 	
 	
+		
+		
+		
+	
+	
+	// Gets input for unit of distance
+	public String readDistanceUnit (Scanner reader) {
+		
+		// Initializes output
+		String output;
+		
+		// Loops until valid
+		do {
+			
+			// Makes call
+			System.out.println("Enter a unit:");
+			output = getDistanceUnit(reader.nextLine());
+			if (output == "invalid") {
+				
+				System.out.println("Input invalid");
+				
+			}
+		}
+		while (output == "invalid");
+		
+		return output;
+		
+	}
+	
 	// Gets unit of distance
 	public String getDistanceUnit (String input) {
+		
+		// Initializes output
 		String output;
+		
 		// Sets output
 		switch (input) {
 		
@@ -60,16 +146,6 @@ public class UnitConverter {
 			case "mi":
 				output = "mi";
 			break;
-			case "pound":
-			case "pounds":
-			case "lb":
-			case "lbs":
-				output = "lb";
-			break;
-			case "ounce":
-			case "ounces":
-			case "oz":
-				output = "oz";
 			case "kilometer":
 			case "kilometre":
 			case "kilometers":
@@ -97,6 +173,9 @@ public class UnitConverter {
 		
 	}
 	
+	// Gets unit of weight
+	public String getWeightUnit ()
+	
 	// Gets unit
 	public String getUnit (String input) {
 		String output;
@@ -123,6 +202,7 @@ public class UnitConverter {
 			case "ounces":
 			case "oz":
 				output = "oz";
+			break;
 			case "kilometer":
 			case "kilometers":
 			case "km":
