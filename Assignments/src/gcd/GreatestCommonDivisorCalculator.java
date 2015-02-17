8cd26879b1a9d5e473d48cdf7b07ca2c9ab9eb5d
Andrew <zhouand000@isd284.com>
2015-02-17 10:19:34 -0600

    Various Changes

---
+++ Assignments/src/gcd/GreatestCommonDivisorCalculator.java
@@ -0,0 +1,69 @@
+package gcd;
+
+import java.util.Scanner;
+
+@SuppressWarnings("javadoc")
+public class GreatestCommonDivisorCalculator {
+	
+	// Creates Scanner
+	Scanner reader;
+	
+	public static void main (String[] args) {
+		
+		// Creates an instance of GreatestCommonDivisorCalculator
+		GreatestCommonDivisorCalculator calc = new GreatestCommonDivisorCalculator();
+		
+		// Initializes Scanner
+		calc.reader = new Scanner(System.in);
+		
+		// Calls gcd method and prints
+		System.out.println("The GCD is: " + calc.getGreatestCommonDivisor(calc.readInt(), calc.readInt()));
+		
+	}
+	
+	// Reads an int
+	public int readInt () {
+		
+		// Initializes output variable
+		int output;
+
+		// Gets input
+		do {
+			
+			System.out.print("Please enter a positive integer: ");
+			output = reader.nextInt();
+			
+		}
+		while (output <= 0);
+		return output;
+		
+	}
+	
+	// Uses Euclid's Algorithm to find the GCD
+	public int getGreatestCommonDivisor (int numberX, int numberY) {
+		
+		// Initializes remainder variable
+		int remainder = -1;
+		
+		// Loops until remainder == 0
+		do {
+			
+			remainder = numberX % numberY;
+			
+			// Checks if the stopping condition is met
+			if (remainder != 0) {
+				
+				numberX = numberY;
+				numberY = remainder;
+				
+			}
+			
+		}
+		while (remainder != 0);
+		
+		// Returns the GCD
+		return numberY;
+		
+	}
+	
+}
