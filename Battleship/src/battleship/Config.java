c8b9cf83506459abc4a9eff550a18891f057cbd9
Andrew <zhouand000@isd284.com>
2015-03-04 10:52:06 -0600

    Added IO

--- Battleship/src/battleship/Config.java
+++ Battleship/src/battleship/Config.java
@@ -1,12 +1,58 @@
 package battleship;
 
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class Config {
 	
-	File config = new File("BattleshipGameConfig.txt");
+	final File config = new File("BattleshipGameConfig.txt");
+	private boolean isNewConfig;
+	private BufferedReader br;
+	private BufferedWriter bw;
+	private Scanner scanner;
+	
+	public String name;
+	
+	
+	
+	
+	public void init(){
+		
+		try {
+			
+			if(config.createNewFile()){
+				
+				isNewConfig = true;
+								
+			}
+			
+			br = new BufferedReader(new FileReader(config));
+			bw = new BufferedWriter(new FileWriter(config));
+			scanner = new Scanner(br);
+			if (isNewConfig){
+				
+				writeln("name = " + name);
+				
+			}
+		}
+		catch (IOException e) {
+			
+			System.err.println("Config could not be created.\n\n");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 	
 	public Config(){
+		
+	}
+	
+	public void writeln(String s) throws IOException{
+		
+		bw.write(s + "\n");
 		
 	}
 	
@@ -14,6 +60,18 @@ public class Config {
 		
 		
 		
+	}
+
+
+
+	public boolean isNewConfig () {
+		return isNewConfig;
+	}
+
+
+
+	public void setNewConfig (boolean isNewConfig) {
+		this.isNewConfig = isNewConfig;
 	}
 	
 	
